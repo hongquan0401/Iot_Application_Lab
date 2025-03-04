aa781e5a0965b8762e1a63e4a1ae177efc4a3e30
Quan Pham <quan.phamvt040103@hcmut.edu.vn>
2025-03-04 16:14:02 +0700

    Change directory

---
+++ include/defines.hpp
@@ -0,0 +1,27 @@
+#ifdef ESP32
+
+// Analog ports
+#define A0 2
+#define A1 4
+#define A2 35
+#define A3 34
+#define A4 36
+#define A5 39
+#define A6 32
+
+// Digital ports
+#define A7 33
+#define D2 26
+#define D3 25
+#define D4 7
+#define D5 16
+#define D6 17
+#define D7 14
+#define D8 12
+#define D9 13
+#define D10 5
+#define D11 23
+#define D12 19
+#define D13 18
+
+#endif
